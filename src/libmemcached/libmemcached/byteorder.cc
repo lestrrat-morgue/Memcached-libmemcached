a14c8f691db6d07dee6ba6a780ee6b846d7bcaf7
Sean Burke <sean@xenadyne.com>
2014-12-18 16:51:30 -0800

    Fix libmemcached-1.0.18 compilation error on MacOSX Xcode 6.1.1

--- src/libmemcached/libmemcached/byteorder.cc
+++ src/libmemcached/libmemcached/byteorder.cc
@@ -38,6 +38,10 @@
 #include "mem_config.h"
 #include "libmemcached/byteorder.h"
 
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+
 /* Byte swap a 64-bit number. */
 #ifndef swap64
 static inline uint64_t swap64(uint64_t in)
